6eb7aaf8acdc12e48179b6464fdb6526449f8598
sadness_ojisan <sadness.ojisan@gmail.com>
2021-01-30 01:08:46 +0900

    wip

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # yew-data-fetch
 
+## spec
+
+use [HackerNews API](https://github.com/HackerNews/API).
+
+The endpoint is https://hacker-news.firebaseio.com/v0/item/8863.json?print=pretty.
+
+## dev
+
 ```
 wasm-pack build --target web --out-name wasm --out-dir ./static
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,36 +1,88 @@
 use wasm_bindgen::prelude::*;
-use yew::prelude::*;
+use yew::{format::{Json, Nothing}, prelude::*, services::{FetchService, fetch::{Request, Response}}};
+use serde::Deserialize;
 
 struct Model {
     link: ComponentLink<Self>,
-    value: i64,
+    data: Option<ResponseData>,
+    loading: bool,
+    error: Option<String>
 }
 
-enum Msg {
-    AddOne,
+#[derive(Deserialize, Debug, Clone)]
+pub struct ResponseData {
+    by : String,
+    descendants : i32,
+    id : i32,
+    kids : Vec<i32>,
+    score : i32,
+    time : i32,
+    title : String,
+    type: String,
+    url : String
 }
+
+
+enum Msg {
+    StartFetchData,
+    SuccessFetchData(Result<ResponseData, anyhow::Error>),
+    FailFetchData(String)
+}
+
+
 
 impl Component for Model {
     type Message = Msg;
     type Properties = ();
     fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
+        link.callback(|_| Msg::StartFetchData);
+
+        let endpoint = "https://hacker-news.firebaseio.com/v0/item/8863.json?print=pretty";
+        let request = Request::get(endpoint)
+        .body(Nothing)
+        .expect("Could not build request.");
+
+        let callback = link.callback(
+            |response: Response<Json<Result<ResponseData, anyhow::Error>>>| {
+                let Json(data) = response.into_body();
+                match data {
+                    ResponseData => {
+                        link.callback(()=> Msg::SuccessFetchData(data));
+                    }
+                    anyhow::Error => Msg::FailFetchData("error")
+                }
+            },
+        );
+        let task = FetchService::fetch(request, callback).expect("failed to start request");
         Self {
             link,
-            value: 0,
+            data: Some(),
+            loading: false,
+            error: Some(String)
         }
     }
 
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
         match msg {
-            Msg::AddOne => self.value += 1
+            Msg::SuccessFetchData(response) => {
+                match response {
+                    Ok(data) => {
+                        self.loading = false;
+                        self.data = Some(data);
+                    }
+                    Err(error) => {
+                        self.loading = false;
+                        self.error = Some(error.to_string());
+                    },
+                }
+                self.fetch_task = None;
+                true
+            }
         }
         true
     }
 
     fn change(&mut self, _props: Self::Properties) -> ShouldRender {
-        // Should only return "true" if new properties are different to
-        // previously received properties.
-        // This component has no properties so we will always return "false".
         false
     }
 
